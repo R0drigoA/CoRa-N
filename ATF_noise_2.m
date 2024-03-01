c059df6a24cd368f94a9db930562b193a92ee070
R0drigoA <rodrigoaguilar@ciencias.unam.mx>
2024-03-01 13:25:50 -0600

    cambios

--- ATF_noise_2.m
+++ ATF_noise_2.m
@@ -1,7 +1,8 @@
 clear; clc;
-rng(1)
+%rng(1)
 
 %% Simulacion sistema ATF  
+
 total_time = 50000;
 
 t = 0;      
@@ -48,15 +49,14 @@ v = [1 0 0 0;
     0 0 1 -1];
 
 
-j1 = 1;    % contador para almacenar el numero de reacciones
+j1 = 1;    % contador para almacenar el numero de ciclos
+
 while t < total_time
     
-    % Perturbacion
     if (t > total_time/2)
        mY = 0.5;
     end
-    
-    % j1p es la reaccion que ocurre (mas o menos) al tiempo de la perturbacion
+
     if (t > (total_time/2)-10 && t < (total_time/2)+10)
        j1p = j1;
     end
@@ -78,6 +78,7 @@ while t < total_time
     l(14) = gU*C;         % U degradates in the complex form 
     l(15) = gW*C;         % W degradates in the complex form 
 
+
     % Calculate total reaction rate
     total_rate = sum(l);
     
@@ -91,16 +92,16 @@ while t < total_time
 
     % Determine which reaction occurs
     r2 = rand();
-    l_rates = l/total_rate;  % vector con cada rate para cada reaccion
-    reaction = 1;            % elejir una reaccion
-    sumR = l_rates(1);       % guardar la suma
+    l_rates = l/total_rate;  
+    reaction = 1;            
+    sumR = l_rates(1);       
 
     while (sumR < r2)        
         reaction = reaction + 1;
         sumR = sumR + l_rates(reaction);
     end
     
-    X = X + v(reaction,:);   % se actualiza el numero de moleculas
+    X = X + v(reaction,:);   % la ultima reaccion del ciclo se suma a X y se actualiza el numero de moleculas
     W = X(1);
     Y = X(2);
     U = X(3);
@@ -149,101 +150,122 @@ legend({"C", "Cp"})
 
 
 
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+%% Estado estacionario de W
 
-%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
-%% Estado estacionario de Y
-n = j1p;
-Y_simulation = X_results(1:j1p,2);          % simulacion de Y
-
-
-Y_molecules = unique(Y_simulation).';   % rango de moleculas de Y
-Y_counts = histcounts(Y_simulation);    % conteo de las moleculas de Y
-Y_prob = Y_counts/n;                    % probabilidad de cada numero de moleculas en la simulacion
+n=j1p;
+W_simulation = X_results(1:j1p,1);          % simulacion de Y
 
 
-for i = 1:length(Y_molecules)
-    Y_cumprob(i) = sum(Y_prob(1:i));
+W_molecules = unique(W_simulation).';   % rango de moleculas de Y
+W_counts = histcounts(W_simulation);    % conteo de las moleculas de Y
+W_prob = W_counts/n;                    % probabilidad de cada numero de moleculas en la simulacion
+
+
+for i = 1:length(W_molecules)
+    W_cumprob(i) = sum(W_prob(1:i));
 end
 
-Y_distribution = [Y_molecules; Y_cumprob];    % distribucion acumulada de Y
+W_distribution = [W_molecules; W_cumprob];    % distribucion acumulada de Y
+
+
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
- 
-%% Simulation of localy analogous system with fixed imput mU = mU*Y_ss
+% 
+%% Simulation of localy analogous system without feedback
+total_time = 50000;
 
 t = 0;
-time_points_analog = zeros(1, 10000000);  
+time_points_analog_v = zeros(1, 10000000);
 
 % Initial conditions
 W = 0;
 Y = 0;
+V = 0;
 U = 0;
 C = 0;
-X = [W, Y, U, C]; 
+X = [W, Y, V, U, C]; 
+X_results_analog_v = zeros(10000000, 5);
 
-X_results_analog = zeros(10000000, 4); 
+% Kinetic parameters
+mY = 0.125;
+gV = 1;
+mV = 0.125;
 
-% Kinetic parameters same as before
-mY = 0.125; % reiniciar mY
+% % estoqiometric matrix
+v_analog = [1 0 0 0 0;
+    0 1 0 0 0;
+    0 0 0 1 0;
+    -1 0 0 -1 1;
+    1 0 0 1 -1;
+    -1 0 0 0 0;
+    0 -1 0 0 0;
+    0 0 -1 0 0;
+    0 0 0 -1 0;
+    0 0 0 0 -1
+    -1 0 0 0 0;
+    0 -1 0 0 0;
+    0 0 -1 0 0;
+    0 0 0 -1 0;
+    0 0 0 0 -1;
+    1 0 0 0 -1;
+    0 0 0 1 -1;
+    0 0 1 0 0];
 
-% Estoqiometric matrix same as before
 
-j2 = 1;
+j3 = 1;
+j = 1;
 while t < total_time
-
     %Perturbation
     if (t>total_time/2)
         mY = 0.5;
     end
 
-    
     if (t > (total_time/2)-10 && t < (total_time/2)+10)
-       j2p = j2;
+       j3p = j3;
     end
 
-    % Un dato aleatoreo de la simulacion anterior
-    % rY = randi(length(Y_simulation));
-    % Y_ss = Y_simulation(rY);
-
-
-    % Usar el vector de la simulacion anterior
-    if(j2 < j1p)
-         Y_ss = Y_simulation(j2);
-    else
-         break     
-         % si el numero de reacciones supera el numero de reacciones de la simulacion 
-         % anterior antes de la perturbacion (j1p) se rompe, aqui no suele
-         % pasar 
-    end
-
-
-    % Dato aleatoreo de la distribucion
-    % rY = rand();
+    % Obtener una W aleatorea de la distribucion
+    % rW = rand();
     % i = 1;
-    % while (Y_distribution(2,i) < rY)
+    % while (W_distribution(2,i) < rW)
     %     i = i +1;
     % end
-    % Y_ss = Y_distribution(1,i);
-    % % 
+    % W_ss = W_distribution(1,i);  
+
+    % seleccionar una W aleatorea de la simulacion anterior
+    rW = randi(j1p);
+    W_ss = W_simulation(rW);
+
+
+    % 
+    % if(j3 < j1p)
+    %     W_ss = W_simulation(j);
+    % else
+    %     break 
+    % % Se necesita primero hacer que la simulacon anterior sea mucho mas
+    % % grande que esta para no romper el ciclo
+    % end
 
 
     % Reaction propencities - lamda
     l(1) = mW;            % M synthesis
     l(2) = mY*W;          % Y synthesis 
-
-    l(3) = mU*Y_ss;          % U synthesis
-
+    l(3) = mU*V;          % U synthesis
     l(4) = np*U*W;        % complex C formation
     l(5) = n0*C;          % complex C spontaneous unbinding rate
     l(6) = gW*W;          % W degradation 
     l(7) = gY*Y;          % Y degradation
-    l(8) = gU*U;          % U degradation
-    l(9) = nm*C;          % C degradation
-    l(10) = g*W;          % W global dilution
-    l(11) = g*Y;          % Y global dilution
-    l(12) = g*U;          % U global dilution
-    l(13) = g*C;          % C global dilution
-    l(14) = gU*C;         % like U degradates in the complex form
-    l(15) = gW*C;         % like W degradates in the complex form
+    l(8) = gV*V;          % V degradation
+    l(9) = gU*U;          % U degradation
+    l(10) = nm*C;         % C degradation
+    l(11) = g*W;          % W global dilution
+    l(12) = g*Y;          % Y global dilution
+    l(13) = g*V;          % V global dilution
+    l(14) = g*U;          % U global dilution
+    l(15) = g*C;          % C global dilution
+    l(16) = gU*C;         % like U degradates in the complex form
+    l(17) = gW*C;         % like W degradates in the complex form
+    l(18) = mV*W_ss;
 
 
     % Calculate total reaction rate
@@ -267,55 +289,62 @@ while t < total_time
         sumR = sumR + l_rates(reaction);
     end
 
-    X = X + v(reaction,:);
+    X = X + v_analog(reaction,:);
     W = X(1);
-    Y = X(2);
-    U = X(3);
-    C = X(4);
+    Y = X(2); 
+    V = X(3);
+    U = X(4);
+    C = X(5);
 
     % Update time and species counts
     t = t + tau;
 
 
-    j2 = j2+1;
+    j3 = j3+1;
     % Store species values and time
-    X_results_analog(j2,:) = X;
-    time_points_analog(j2) = t;
+    X_results_analog_v(j3,:) = X;
+    time_points_analog_v(j3) = t;
 end
 
 
 
-
-% Plot the results for each species
+% 
+% % Plot the results for each species
 tiledlayout(2,1)
 nexttile
-plot(time_points_analog(1:j2), X_results_analog((1:j2),:), 'LineWidth', 0.25);
+plot(time_points_analog_v(1:j3), X_results_analog_v((1:j3),:), 'LineWidth', 0.25);
 xlim([0 total_time]);
-legend({"W", "Y", "U", "C"})
+legend({"W", "Y", "V", "U", "C"})
 nexttile
-plot(time_points_analog(1:j2), X_results_analog((1:j2),2), 'LineWidth', 0.25);
+plot(time_points_analog_v(1:j3), X_results_analog_v((1:j3),2), 'LineWidth', 0.25);
 xlim([0 total_time]);
 legend({"Y"})
 
 
-tiledlayout(2,2)
-nexttile;   
-histogram(X_results_analog(1:j2p,1), 'FaceAlpha',0.3, 'Normalization', 'probability');
+tiledlayout(2,3)
+nexttile;
+histogram(X_results_analog_v(1:j3p,1), 'FaceAlpha',0.3, 'Normalization', 'probability');
 hold on
-histogram(X_results_analog(j2p:j2,1), 'FaceAlpha',0.3, 'Normalization', 'probability');
+histogram(X_results_analog_v(j3p:j3,1), 'FaceAlpha',0.3, 'Normalization', 'probability');
 legend({"W", "Wp"})
 nexttile
-histogram(X_results_analog(1:j2p,2), 'FaceAlpha',0.3, 'Normalization', 'probability');
+histogram(X_results_analog_v(1:j3p,2), 'FaceAlpha',0.3, 'Normalization', 'probability');
 hold on
-histogram(X_results_analog(j2p:j2,2), 'FaceAlpha',0.3, 'Normalization', 'probability');
+histogram(X_results_analog_v(j3p:j3,2), 'FaceAlpha',0.3, 'Normalization', 'probability');
 legend({"Y", "Yp"})
 nexttile
-histogram(X_results_analog(1:j2p,3), 'FaceAlpha',0.3, 'Normalization', 'probability', NumBins=max(X_results_analog(1:j2p,3)));
+histogram(X_results_analog_v(1:j3p,3), 'FaceAlpha',0.3, 'Normalization', 'probability');
 hold on
-histogram(X_results_analog(j2p:j2,3), 'FaceAlpha',0.3, 'Normalization', 'probability');
+histogram(X_results_analog_v(j3p:j3,3), 'FaceAlpha',0.3, 'Normalization', 'probability');
+legend({"V", "Vp"})
+nexttile
+histogram(X_results_analog_v(1:j3p,4), 'FaceAlpha',0.3, 'Normalization', 'probability');
+hold on
+histogram(X_results_analog_v(j3p:j3,4), 'FaceAlpha',0.3, 'Normalization', 'probability');
 legend({"U", "Up"})
 nexttile
-histogram(X_results_analog(1:j2p,4), 'FaceAlpha',0.3, 'Normalization', 'probability');
+histogram(X_results_analog_v(1:j3p,5), 'FaceAlpha',0.3, 'Normalization', 'probability');
 hold on
-histogram(X_results_analog(j2p:j2,4), 'FaceAlpha',0.3, 'Normalization', 'probability');
+histogram(X_results_analog_v(j3p:j3,5), 'FaceAlpha',0.3, 'Normalization', 'probability');
 legend({"C", "Cp"})
+
